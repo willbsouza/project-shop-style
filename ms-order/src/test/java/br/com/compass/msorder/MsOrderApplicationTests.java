1d9ffaf78603b3ecf8d5fe10438c36ae1b04d02c
Willams Souza <willamsbernardo@hotmail.com>
2022-06-23 17:52:30 -0300

    Fourth Microservice (Order) added.

---
+++ ms-order/src/test/java/br/com/compass/msorder/MsOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.msorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
