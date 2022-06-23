a12e51db640d1e80f317d449bee47e04b0dc8115
Willams Souza <willamsbernardo@hotmail.com>
2022-06-23 14:43:59 -0300

    Adding @JoinColumn(name = category_id) in the Product entity.

--- ms-catalog/src/main/java/br/com/compass/mscatalog/entity/Product.java
+++ ms-catalog/src/main/java/br/com/compass/mscatalog/entity/Product.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotEmpty;
@@ -39,6 +40,7 @@ public class Product {
 	private List<Sku> skus;
 	
 	@ManyToOne
+	@JoinColumn(name = "category_id")
 	@NotNull
 	private Category category;
 	
