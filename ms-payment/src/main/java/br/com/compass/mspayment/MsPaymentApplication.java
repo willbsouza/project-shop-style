3ae26d4eb19fe3f9c31950be6d1f996a02cb3877
Willams Souza <willamsbernardo@hotmail.com>
2022-06-23 13:51:19 -0300

    Adding Swagger and changing version Spring Boot to 2.5.11

--- ms-payment/src/main/java/br/com/compass/mspayment/MsPaymentApplication.java
+++ ms-payment/src/main/java/br/com/compass/mspayment/MsPaymentApplication.java
@@ -3,7 +3,10 @@ package br.com.compass.mspayment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class MsPaymentApplication {
 
 	public static void main(String[] args) {
