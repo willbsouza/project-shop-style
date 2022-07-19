904c18c69c2149bca6ec0e7a3d8b5b033ff90281
Willams Souza <willamsbernardo@hotmail.com>
2022-07-19 12:26:02 -0300

    Adding authentication security between Microservices with Eureka Server.

---
+++ eureka-server/src/main/java/br/com/compass/eureka/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package br.com.compass.eureka.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.csrf()
+		.disable()
+		.authorizeRequests()
+		.anyRequest().authenticated()
+		.and()
+		.httpBasic();
+	}
+}
