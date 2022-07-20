365bf6e1bb5912685e8d1379f61e0f62a86c31a1
Willams Souza <willamsbernardo@hotmail.com>
2022-07-20 13:54:17 -0300

    Updating SecurityConfig class, because WebSecurityConfigurerAdapter was deprecated.

--- eureka-server/src/main/java/br/com/compass/eureka/config/SecurityConfig.java
+++ eureka-server/src/main/java/br/com/compass/eureka/config/SecurityConfig.java
@@ -1,14 +1,15 @@
 package br.com.compass.eureka.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 		.csrf()
 		.disable()
@@ -16,5 +17,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.anyRequest().authenticated()
 		.and()
 		.httpBasic();
+		return http.build();
 	}
 }
